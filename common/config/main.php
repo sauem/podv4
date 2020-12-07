c8228a15e76f6c3e65d20ed1482b4cc3d8f8354c
sauem <nguyendinhthang.go97@gmail.com>
2020-12-07 11:41:22 +0700

    update

---
+++ common/config/main.php
@@ -0,0 +1,50 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm' => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'modules' => [
+        'rbac' => [
+            'class' => 'mdm\admin\Module'
+        ]
+    ],
+    'components' => [
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager', // or use 'yii\rbac\DbManager'
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => SQL_HOST,
+            'username' => SQL_USER_NAME,
+            'password' => SQL_PASSWORD,
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            // Duration of schema cache.
+            'schemaCacheDuration' => 3600,
+            // Name of the cache component used to store schema information
+            'schemaCache' => 'cache',
+            'queryCache' => 'cache',
+            'enableQueryCache' => true,
+        ],
+        'view' => [
+            'class' => 'yii\web\View',
+            'renderers' => [
+                'blade' => [
+                    'class' => '\cyneek\yii2\blade\ViewRenderer',
+                    'cachePath' => '@runtime/blade_cache',
+                ],
+            ],
+        ],
+        'as access' => [
+            'class' => 'mdm\admin\components\AccessControl',
+            'allowActions' => [
+                'rbac/*',
+            ]
+        ],
+    ],
+];
---
+++ env.example.php
@@ -0,0 +1,30 @@
+<?php
+
+define("SQL_HOST", "mysql:host=127.0.0.1;dbname=podv3.local");
+define("SQL_USER_NAME", "sauem");
+define("SQL_PASSWORD", "123");
+
+define("REDIS_HOST", "127.0.0.1");
+define("REDIS_PORT", 6379);
+define("REDIS_PASS", "");
+define("REDIS_USER", "");
+
+
+define('CACHE_ENABLE', true);
+define('CACHE_DURATION', 900);
+
+
+define('EMAIL_DOMAIN', 'mail.purrcreative.vn');
+define('EMAIL_PRIVATE_KEY', '27e6c8a174e12ebb96f3368272418b10-c27bf672-3390aa1d');
+
+define("COOKIE_VALID_BACKEND", "X0XA3Wzms68GfLNLtbW1");
+define("COOKIE_VALID_FRONTEND", "c9eBMdt5xbEiroK8brAV");
+
+define("UPLOAD_PATH", "D:/project/podv3/uploads/");
+
+define("FILESYSTEM_CLOUD", "google");
+define("GOOGLE_DRIVE_CLIENT_ID", "406123247192-btg4gtt9a65j1s4oo6cqcromjl2utlth.apps.googleusercontent.com");
+define("GOOGLE_DRIVE_CLIENT_SECRET", "ZVELYK2PKIMHD1ZCf2DFePpw");
+define("GOOGLE_DRIVE_REFRESH_TOKEN", "1//0f91hlIL-P32LCgYIARAAGA8SNwF-L9IrVF14VRrXxhXYGfEd0vIwXoHGNlHvrK9yC_ytityaJbDvE7i3wHL_bmTy9iXDlnt-450");
+define("GOOGLE_DRIVE_FOLDER_ID", "1Oedp5Y-XxpFCU99NjYWc6RL2xVtfI6kR");
+
