a81fe31d58a38b9bc13cd38912c11c8622e39c4f
sauem <nguyendinhthang.go97@gmail.com>
2021-01-05 11:16:29 +0700

    cross order

--- backend/controllers/OrderCrossController.php
+++ backend/controllers/OrderCrossController.php
@@ -9,7 +9,9 @@ use backend\models\OrdersContact;
 use backend\models\OrdersContactSearch;
 use backend\models\OrderStatus;
 use common\helper\Helper;
+use yii\data\ActiveDataProvider;
 use yii\db\Transaction;
+use yii\web\BadRequestHttpException;
 
 class OrderCrossController extends BaseController
 {
@@ -49,17 +51,40 @@ class OrderCrossController extends BaseController
     {
         $model = new OrderCrossStatus();
         $ids = \Yii::$app->request->get('codes');
+        $transaction = \Yii::$app->getDb()->beginTransaction(Transaction::SERIALIZABLE);
         if (empty($ids)) {
             return static::responseSuccess(0, 1);
         }
 
-        $searchModel = new OrdersContactSearch();
-        $dataProvider = $searchModel->search([
-            'OrdersContactSearch' => [
-                'id' => $ids,
-//                'payment_status' => [OrdersContact::STATUS_PAYED, OrdersContact::STATUS_REFUND],
-//                'status' => OrdersContact::STATUS_PAYED
-            ]
+        if (\Yii::$app->request->isPost && $model->load(\Yii::$app->request->post())) {
+            try {
+                foreach ($ids as $id) {
+                    $order = OrdersContact::findOne($id);
+                    if (!$order) {
+                        throw new BadRequestHttpException("Không tìm thấy đơn hàng!");
+                    }
+                    $order->cross_check_code = OrderCrossStatus::makeCode(
+                        $model->remittance_date,
+                        $model->country,
+                        $model->partner,
+                        $model->cross_count
+                    );
+                    $order->remittance_date = Helper::timer(str_replace("/", '-', $model->remittance_date));
+                    $order->cross_status = OrdersContact::STATUS_CROSSED;
+                    $order->status = OrdersContact::STATUS_CROSSED;
+                    if (!$order->save()) {
+                        throw new BadRequestHttpException(Helper::firstError($order));
+                    }
+                }
+                $transaction->commit();
+                return static::responseSuccess(1, 1, 'Thao tác thành công!');
+            } catch (\Exception $exception) {
+                $transaction->rollBack();
+                \Yii::$app->session->setFlash("danger", $exception->getMessage());
+            }
+        }
+        $dataProvider = new ActiveDataProvider([
+            'query' => OrdersContact::find()->where(['id' => $ids])
         ]);
         return static::responseRemote("make-cross.blade", [
             "model" => $model,
