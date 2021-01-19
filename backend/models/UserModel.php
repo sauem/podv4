e4b69dd2e42b0a503dfbf8f615ec0dca0f660cff
sauem <nguyendinhthang.go97@gmail.com>
2021-01-19 15:33:48 +0700

    update view total

--- backend/models/UserModel.php
+++ backend/models/UserModel.php
@@ -257,7 +257,9 @@ class UserModel extends BaseModel implements IdentityInterface
 
     public function getPosition()
     {
-        return $this->hasOne(AuthAssignment::className(), ['user_id' => 'id']);
+        return $this->hasOne(AuthAssignment::className(), ['user_id' => 'id'])->where([
+            '{{auth_item}}.type' => 1
+        ]);
     }
 
     public function afterFind()
