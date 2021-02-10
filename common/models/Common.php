f8defe848358429cb74dc77c3d29f66664d0189d
sauem <nguyendinhthang.go97@gmail.com>
2021-02-10 11:05:45 +0700

    load more

--- common/models/Common.php
+++ common/models/Common.php
@@ -12,18 +12,24 @@ class Common extends Model
     public $sheet_id;
     public $phone_prioritize_time;
     public $callback_to_cancel_time;
+    public $description;
+    public $sitename;
+    public $notify;
 
     public function rules()
     {
         return [
             [['callback_to_cancel_time'], 'number'],
-            [['phone_prioritize_time', 'sheet_id',], 'string']
+            [['phone_prioritize_time', 'sheet_id', 'sitename', 'description', 'notify'], 'string']
         ];
     }
 
     public function attributeLabels()
     {
         return [
+            'sitename' => 'Tên hệ thống',
+            'description' => 'Mô tả hệ thống',
+            'notify' => 'Thông báo chung',
             'sheet_id' => 'ID google sheet backup contact',
             'phone_prioritize_time' => 'Thời gian ưu tiên số mới (ngày)',
             'callback_to_cancel_time' => 'Thời gian hủy số gọi lại (ngày)',
