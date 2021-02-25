27b773b44a5adfc8494365b3513efea8e9b2c5e2
sauem <nguyendinhthang.go97@gmail.com>
2021-02-25 16:17:27 +0700

    update

--- backend/controllers/ReportController.php
+++ backend/controllers/ReportController.php
@@ -57,8 +57,8 @@ class ReportController extends BaseController
             ->from('orders_contact as O')
             ->where(['O.cross_status' => OrdersContact::STATUS_CROSSED])
             //->andWhere(['=', 'O.payment_status', OrdersContact::STATUS_PAYED])
-            ->orWhere(['=', 'O.status', OrdersContact::STATUS_CROSSED])
-            ->orWhere(['=', 'O.shipping_status', OrdersContact::STATUS_REFUND])
+//            ->orWhere(['=', 'O.status', OrdersContact::STATUS_CROSSED])
+//            ->orWhere(['=', 'O.shipping_status', OrdersContact::STATUS_REFUND])
             ->addSelect([
                 'id',
                 'name',
