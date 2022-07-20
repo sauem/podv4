401bc6056e3410665f16c8d29ece926b398e5ca2
sauem <nguyendinhthang.go97@gmail.com>
2022-07-20 23:06:28 +0700

    asdas

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -9,3 +9,5 @@ Yii::setAlias('@mdm/admin/controllers', dirname(dirname(__DIR__)) . '/vendor/mdm
 Yii::setAlias('@mdm/admin/components', dirname(dirname(__DIR__)) . '/vendor/mdmsoft/yii2-admin/components');
 Yii::setAlias('@mdm/admin/messages', dirname(dirname(__DIR__)) . '/vendor/mdmsoft/yii2-admin/messages');
 Yii::setAlias('@mdm/admin/migrations', dirname(dirname(__DIR__)) . '/vendor/mdmsoft/yii2-admin/migrations');
+Yii::setAlias('@mdm/admin', dirname(dirname(__DIR__)) . '/vendor/mdmsoft/yii2-admin');
+Yii::setAlias('@mdm/admin/*', dirname(dirname(__DIR__)) . '/vendor/mdmsoft/yii2-admin/*');
